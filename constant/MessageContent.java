2f1d83b13eec65a2f2cd113f4a98c09a23321ced
loumac91 <loumac91@googlemail.com>
2020-05-17 13:41:14 +0100

    delete before final refactor and comments

--- constant/MessageContent.java
+++ constant/MessageContent.java
@@ -4,7 +4,7 @@ public class MessageContent {
 
   public class Pattern {
     
-    public static final String CURRENT_TIME_QUERY = "(know the time?|whats the time|what's the time|what time is it|what is the time|wat's the time|wat time is it|wat is the time)";
+    public static final String CURRENT_TIME_QUERY = "(know the time?|whats the time|what's the time|what time is it|what is the time|wat's the time|wat time is it|wat is the time|wats the time|tell me the time)";
     public static final String CURRENT_DAY_QUERY = "(what day is it|wat day is it)";    
   }
 }
